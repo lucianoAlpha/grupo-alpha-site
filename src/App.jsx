fe743365cded82368f275f05aee70bd1b6d30156
lucianoAlpha <luciano.bsr@outlook.com>
2025-05-15 18:02:23 +0100

    Remove Router duplicado do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PaginaImovel from './pages/PaginaImovel';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/imovel/:id" element={<PaginaImovel />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/imovel/:id" element={<PaginaImovel />} />
+    </Routes>
   );
 }
 
