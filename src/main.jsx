7b235b742a8f42cb78ad5813f374a3b835ddf551
lucianoAlpha <luciano.bsr@outlook.com>
2025-05-15 17:37:20 +0100

    Corrige main.jsx para usar App.jsx com rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Home from './pages/Home'
-import Imovel from './pages/Imovel'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/imovel/:id" element={<Imovel />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
