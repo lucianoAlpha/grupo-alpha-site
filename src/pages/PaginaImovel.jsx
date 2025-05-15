82e54b7385206cd7524e7e93ccdeb9f117d94df9
lucianoAlpha <luciano.bsr@outlook.com>
2025-05-15 17:48:26 +0100

    Corrige importação de imoveis no PaginaImovel.jsx

--- src/pages/PaginaImovel.jsx
+++ src/pages/PaginaImovel.jsx
@@ -1,18 +1,21 @@
+// src/pages/PaginaImovel.jsx
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Imovel from './Imovel';
-import imoveis from '../data/imoveis';
+import { imoveis } from '../data/imoveis';
 import styles from './PaginaImovel.module.css';
 
 function PaginaImovel() {
   const { id } = useParams();
   const imovel = imoveis.find((item) => item.id === parseInt(id));
 
+  if (!imovel) {
+    return <p>Imóvel não encontrado.</p>;
+  }
+
   return (
-    <div className={styles.pageContainer}>
-      <div className={styles.card}>
-        <Imovel imovel={imovel} />
-      </div>
+    <div className={styles.container}>
+      <Imovel imovel={imovel} />
     </div>
   );
 }
